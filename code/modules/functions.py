4b566e0d662f2bbcfa6ba9d98f9a0950004e8270
lorenzgewert <lorenz.gewert@student.uni-tuebingen.de>
2022-12-09 11:47:48 +0100

    analysis 1st version

--- code/modules/functions.py
+++ code/modules/functions.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
+
+#test
 
 def exec_timer(function):
     """
@@ -65,3 +68,34 @@ def bound_logistic(lowerb=None, upperb=None):
         return lowerb + (upperb-lowerb)*(1/(1 + np.exp(-k*(x-x0))))
 
     return logistic
+
+#analyse für graphiken
+cpds = [2,8]
+def analysis(df, cpds)
+
+    for i in cpds:
+        subdf = df[df.cpd == i]
+        resp_time_mean = np.array(subdf.tordur.groupby(df.delay).mean())
+        resp_time_std = np.array(subdf.tordur.groupby(df.delay).std())
+        shifts_mean = np.array(subdf.switches.groupby(df.delay).mean())
+        shifts_std = np.array(subdf.switches.groupby(df.delay).std())
+        proc_time_mean = np.array(subdf.tpertrial.groupby(df.delay).mean())
+        proc_time_std = np.array(subdf.tpertrial.groupby(df.delay).std())
+
+        # error rate (müsste dann eigentlich der median sein, wenn wir so wenige subjects haben)
+        error_rates = np.array()
+        for k in [1:(len(df.subj)/60+1)]
+            subsubdf = subdf[subdf.subj == VPk]
+            error_rate = np.array(subsubdf.error.groupby(df.delay).sum()) / 15
+            error_rates.append(error_rate)
+
+        error_rate_median = np.median(error_rates)
+
+    #hier arrays an analyse-dataframe anfügen
+
+
+
+    return (analyse-dataframe)
+
+#es fehlt: anständiger loop für error rate (wie durch subj iterieren?)
+#          sinnvoller return -> ein dataframe mit 2 dimensionen (2 & 8 cpds), der dann darin die jeweils 4 arrays aus dem loop vorhält
