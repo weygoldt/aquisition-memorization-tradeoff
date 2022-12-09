7c369701959f29cea0f5821a532483fbde61024c
lorenzgewert <lorenz.gewert@student.uni-tuebingen.de>
2022-12-09 15:39:54 +0100

    analysis 1st version

--- code/analsysistest.py
+++ code/analsysistest.py
@@ -5,24 +5,46 @@ from IPython import embed
 
 df = pd.read_csv('../data_processed/cvs.csv')
 
+twocpds = pd.DataFrame()
+twocpds.columns = [
+            "resp_time_mean",
+            "resp_time_std",
+            "shifts_mean",
+            "shifts_std",
+            "proc_time_mean",
+            "proc_time_std",
+            "error_rate_median"
+        ]
+eightcpds = pd.DataFrame()
+eightcpds.columns = [
+            "resp_time_mean",
+            "resp_time_std",
+            "shifts_mean",
+            "shifts_std",
+            "proc_time_mean",
+            "proc_time_std",
+            "error_rate_median"
+        ]
+
 cpds = [2,8]
-for i in cpds:
+store = [twocpds, eightcpds]
+for i in cpds and d in store:
 
     subdf = df[df.cpd == i]
 
-    resp_time_mean = np.array(subdf.totdur.groupby(df.delay).mean())
-    resp_time_std = np.array(subdf.totdur.groupby(df.delay).std())
-    shifts_mean = np.array(subdf.switches.groupby(df.delay).mean())
-    shifts_std = np.array(subdf.switches.groupby(df.delay).std())
-    proc_time_mean = np.array(subdf.tpertrial.groupby(df.delay).mean())
-    proc_time_std = np.array(subdf.tpertrial.groupby(df.delay).std())
+    d.resp_time_mean = np.array(subdf.totdur.groupby(df.delay).mean())
+    d.resp_time_std = np.array(subdf.totdur.groupby(df.delay).std())
+    d.shifts_mean = np.array(subdf.switches.groupby(df.delay).mean())
+    d.shifts_std = np.array(subdf.switches.groupby(df.delay).std())
+    d.proc_time_mean = np.array(subdf.tpertrial.groupby(df.delay).mean())
+    d.proc_time_std = np.array(subdf.tpertrial.groupby(df.delay).std())
 
     #print(subdf)
     # error rate (müsste dann eigentlich der median sein, wenn wir so wenige subjects haben)
 
     error_rates = []
     #Problem: Wenn wir einfach name in subdf.subj schreiben kriegen wir 30 Ergebnisse pro VP
-    for name in ['eva','Lorenz','Patrick']:
+    for name in ['eva','Lorenz','Patrick', 'VP2', 'VP3', 'VP4', 'VP5', 'VP6']:
         subsubdf = subdf[subdf.subj == name]
         error_rate = (np.array(subsubdf.iserror.groupby(df.delay).sum()) / 15) * 100
         error_rates.append(error_rate)
@@ -30,8 +52,11 @@ for i in cpds:
         #print(error_rates)
         #embed()
     #error_rates = np.array(error_rates)
-    error_rate_median_0 = np.median(error_rates[0])
-    error_rate_median_2 = np.median(error_rates[1])
+    #error_rate_median_0 = np.median(error_rates[0])
+    #error_rate_median_2 = np.median(error_rates[1])
+
+    d.error_rate_median = np.median(error_rates)
+
     print(error_rates)
     print(error_rate_median_0)
     print(error_rate_median_2)
