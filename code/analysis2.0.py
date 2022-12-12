9d7439d728440877ab59d14e94773ede7fee41c3
lorenzgewert <lorenz.gewert@student.uni-tuebingen.de>
2022-12-12 10:19:25 +0100

    analysis 2nd version

--- code/analysis2.0.py
+++ code/analysis2.0.py
@@ -6,13 +6,13 @@ from IPython import embed
 df = pd.read_csv('../data_processed/cvs.csv')
 
 resp_time = dict({"cpds": [2,2,8,8], "delay": [0,2,0,2], "median": []})
+print(resp_time)
 
 cpds = [2,8]
 
 for i in cpds:
     subdf = df[df.cpd == i]
 
-    d.resp_time_median = np.array(subdf.totdur.groupby(df.delay).median())
-    resp_time["median"].append(d.resp_time_median[0],d.resp_time_median[1]
-
-    print(resp_time)
+    resp_time_median = np.array(subdf.totdur.groupby(df.delay).median())
+    print(resp_time_median)
+    resp_time["median"].append(resp_time_median[0],d.resp_time_median[1]
