5ecb10eee7294e2eb3d0941af8b2bb27a991dcca
weygoldt <88969563+weygoldt@users.noreply.github.com>
2022-12-12 21:52:36 +0100

    some work

--- code/plot_cvs_powerfit_delay.py
+++ code/plot_cvs_powerfit_delay.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+import cmocean
 import cmocean.cm as cmo
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
@@ -9,6 +10,7 @@ import pandas as pd
 import plottools.colors as clrs
 import seaborn as sns
 from matplotlib.pyplot import cm
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
 from scipy.optimize import curve_fit
 from scipy.stats import wilcoxon
 
@@ -24,12 +26,17 @@ ps = PlotStyle()
 dataroot = Path("../data/cvs")
 processed_dataroot = Path("../data_processed")
 df = dataimport(dataroot, processed_dataroot)
-colors = mcolors.TABLEAU_COLORS
+
+newcmap = cmocean.tools.crop_by_percent(cmo.ice, 20, which='max', N=None)
+colors = newcmap(np.linspace(0, 1, 4))
+
+newcmap = cmocean.tools.crop_by_percent(cmo.haline, 20, which='max', N=None)
+colors2 = newcmap(np.linspace(0,1,4))
 
 
 def plot_errorbars(df, ax, color):
 
-    for name, _ in zip(df.names, colors):
+    for name in df.names: 
         minidf = df[df.names == name]
         ax.errorbar(
             minidf.switches_means,
@@ -52,12 +59,24 @@ ms_2 = mean_sem(df_2)
 popt0, pcov = curve_fit(func_powerlaw, ms_0.switches_means, ms_0.tpertrial_means, method="trf", maxfev=50000)
 popt2, pcov = curve_fit(func_powerlaw, ms_2.switches_means, ms_2.tpertrial_means, method="trf", maxfev=50000)
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(12*ps.cm, 12*ps.cm))
 plot_errorbars(ms_0, ax, color='red')
 plot_errorbars(ms_2, ax, color='blue')
 
 x = np.linspace(ms_2.switches_means.min()-0.4, ms_2.switches_means.max()+2, 1000)
 ax.plot(x, func_powerlaw(x, *popt0))
 ax.plot(x, func_powerlaw(x, *popt2))
+
+axins = inset_axes(ax, 3, 2.5, loc=1)
+plot_errorbars(ms_0, axins, color=colors[1])
+plot_errorbars(ms_2, axins, color=colors[3])
+axins.plot(x, func_powerlaw(x, *popt0), color=colors2[2], zorder=10)
+axins.plot(x, func_powerlaw(x, *popt2), color=colors2[3], zorder=10)
+
+x1, x2, y1, y2 = 3, 15, 1, 5
+axins.set_xlim(x1, x2)
+axins.set_ylim(y1, y2)
+axins.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
+mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
 
 plt.show()
