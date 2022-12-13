34b8adac57c2009ac10f3452a4afa48913773163
lorenzgewert <lorenz.gewert@student.uni-tuebingen.de>
2022-12-13 17:16:15 +0100

    fehlt: proc_time, resp_time, err_rate

--- code/Wilcoxon.py
+++ code/Wilcoxon.py
@@ -16,21 +16,39 @@ proc_time = pd.read_csv('proc_time.csv', header=0)
 shifts = pd.read_csv('shifts.csv', header=0)
 #shifts.drop(["NaN"], inplace=True)
 
-print(shifts)
+#print(shifts)
 cpds = [2,8]
 names = ['eva','Lorenz','Patrick', 'Sofie', 'VP2', 'vp4', 'VP5', 'vp6', 'VP7']
 
-shifts_cpds = dict({"cpds": [2,2,2,2,2,8,8,8,8,8], "mean": [], "subj": []})
+shifts_cpds = dict({"cpds": [2,2,2,2,2,2,2,2,2,8,8,8,8,8,8,8,8,8], "mean": [], "subj": []})
 
 for i in cpds:
     subdf = shifts[shifts.cpds == i]
 
+    # Wilcoxon signed rank
+    delay0 = subdf[subdf['delay'] == 0]
+    delay2 = subdf[subdf['delay'] == 2]
+    stats = wilcoxon(delay0['mean'], delay2['mean'])
+    print(f'shifts, delay {i}cpds')
+    print(stats)
+
     for name in names:
         subsubdf = subdf[subdf.subj == name]
 
-        mean = np.array(subsubdf['mean'].mean())
-        shifts["mean"].append(mean)
-        shifts["subj"].append(name)
-        print(name)
-        print(mean)
+        mean = np.mean(subsubdf['mean'])
+        #print(mean)
+        shifts_cpds['mean'].append(mean)
+        shifts_cpds['subj'].append(name)
+
 print(shifts_cpds)
+
+shifts = pd.DataFrame(shifts_cpds)
+#shifts.to_csv('shifts_cpds.csv')
+
+#Wilcoxon signed rank
+cpds2 = shifts[shifts['cpds'] == 2]
+cpds8 = shifts[shifts['cpds'] == 8]
+
+stats = wilcoxon(cpds2['mean'], cpds8['mean'])
+print('shifts, cpds')
+print(stats)
